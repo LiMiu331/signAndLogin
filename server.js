990c2ecf84e09d1e2f86a6e82e0f5f537be669fb
libb <boomlibb1996@gmail.com>
2020-03-08 23:12:11 +0800

    cookie

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
@@ -12,7 +12,9 @@ var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
   var pathWithQuery = request.url
   var queryString = ''
-  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) {
+    queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'))
+  }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
@@ -23,55 +25,149 @@ var server = http.createServer(function (request, response) {
 
   if (path === '/') {
     let string = fs.readFileSync('./index.html', 'utf-8')
+    let cookies = request.headers.cookie.split('; ')
+    let hash = {}
+    for (let i = 0; i < cookies.length; i++){
+      let parts = cookies[i].split('=')
+      let key = parts[0]
+      let value = parts[1]
+      hash[key] = value
+    }
+    let email = hash.sign_in_email
+    let users = fs.readFileSync('./db/users', 'utf-8')
+    users = JSON.parse(users)
+    let founduser
+    for (let i = 0; i < users.length; i++){
+      if(users[i].email === email){
+          founduser = users[i]
+        break
+      }
+    }
+    if (founduser) {
+      string = string.replace('2222', founduser.password)
+    } else {
+      string = string.replace('2222', '不知道')
+    }
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path === '/sign_up' && method ==='GET'){
-  	let string = fs.readFileSync('./sign_up.html', 'utf-8')
-  	response.statusCode = 200
-  	response.setHeader('Content-Type', 'text/html;charset=utf-8')
-  	response.write(string)
-  	response.end()
+  } else if (path === '/sign_up' && method === 'GET') {
+    let string = fs.readFileSync('./sign_up.html', 'utf-8')
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(string)
+    response.end()
   } else if (path === '/sign_up' && method === 'POST') {
-    let body = [];//请求体
+    let body = []; //请求体
     request.on('data', (chunk) => {
       body.push(chunk)
     }).on('end', () => {
       body = Buffer.concat(body).toString();
-      let strings = body.split('&')  //['email=1','password=2','password_confirmation=3']
+      let strings = body.split('&') //['email=1','password=2','password_confirmation=3']
       let hash = {}
       strings.forEach((string) => {
         let parts = string.split('=')
         let key = parts[0]
         let value = parts[1]
-        hash[key] = value
+        hash[key] = decodeURIComponent(value)
       })
-      let { email, password, password_confirmation } = hash 
+      let {
+        email,
+        password,
+        password_confirmation
+      } = hash
       if (email.indexOf('@') === -1) {
         response.statusCode = 400
         response.setHeader('Content-Type', 'application/json;charset=utf-8')
         response.write(`{
           "errors":{
             "email":"invalid"
-          }}`)
-      }else if(password !== password_confirmation){
+          }
+        }`)
+      } else if (password !== password_confirmation) {
         response.statusCode = 400
         response.write('password not match')
-      }else {
-        response.statusCode = 200
+      } else {
+        var users = fs.readFileSync('./db/users', 'utf8')
+        users = JSON.parse(users)
+        let inUse = false
+        for (let i = 0; i < users.length; i++) {
+          let user = users[i]
+          if (user.email === email) {
+            inUse = true
+            break;
+          }
+        }
+        if (inUse) {
+          response.statusCode = 400
+          response.write('email in use')
+        } else {
+          users.push({
+            email: email,
+            password: password
+          })
+          var usersString = JSON.stringify(users)
+          fs.writeFileSync('./db/users', usersString)
+          response.statusCode = 200
+        }
+
       }
       response.end()
     })
 
-  }else if (path === '/main.js') {
-    let string = fs.readFileSync('./main.js', 'utf-8')
+  } else if(path === '/sign_in' && method === 'GET') {
+    let string = fs.readFileSync('./sign_in.html', 'utf-8')
     response.statusCode = 200
-    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
-  }
-  else if (path === '/xxx') {
+  } 
+  else if (path === '/sign_in' && method === 'POST') {
+    let body = []; //请求体
+    request.on('data', (chunk) => {
+      body.push(chunk)
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      let strings = body.split('&') //['email=1','password=2','password_confirmation=3']
+      let hash = {}
+      strings.forEach((string) => {
+        let parts = string.split('=')
+        let key = parts[0]
+        let value = parts[1]
+        hash[key] = decodeURIComponent(value)
+      })
+      let {
+        email,
+        password
+      } = hash
+      console.log(email)
+      console.log(password)
+
+      var users = fs.readFileSync('./db/users', 'utf8')
+      users = JSON.parse(users)
+      let found
+      for (let i = 0; i < users.length; i++){
+        if (users[i].email === email && users[i].password === password) {
+          found = true
+          break
+        } 
+      }
+      if (found) {
+        response.setHeader('Set-Cookie',`sign_in_email=${email}`)
+        response.statusCode = 200
+      } else {
+        response.statusCode = 401
+      }
+      response.end()
+      })
+    }else if (path === '/main.js') {
+      let string = fs.readFileSync('./main.js', 'utf-8')
+      response.statusCode = 200
+      response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+      response.write(string)
+      response.end()
+    }else if (path === '/xxx') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/json;charset=utf-8')
     response.write(
