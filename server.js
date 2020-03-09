9f7623a5a8693cfe63b2a1c4bb4fa6d7b248c994
libb <845217637@qq.com>
2020-03-09 16:32:24 +0800

    localStorage

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ if (!port) {
   process.exit(1)
 }
 
+let sessions = {
+
+}
+
 var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
   var pathWithQuery = request.url
@@ -25,7 +29,10 @@ var server = http.createServer(function (request, response) {
 
   if (path === '/') {
     let string = fs.readFileSync('./index.html', 'utf-8')
-    let cookies = request.headers.cookie.split('; ')
+    let cookies =''
+    if (request.headers.cookie) {
+       cookies = request.headers.cookie.split('; ')
+    }
     let hash = {}
     for (let i = 0; i < cookies.length; i++){
       let parts = cookies[i].split('=')
@@ -33,7 +40,11 @@ var server = http.createServer(function (request, response) {
       let value = parts[1]
       hash[key] = value
     }
-    let email = hash.sign_in_email
+    let mySession = sessions[hash.sessionId]
+    let email
+    if (mySession) {
+      email= mySession.sign_in_email
+    }
     let users = fs.readFileSync('./db/users', 'utf-8')
     users = JSON.parse(users)
     let founduser
@@ -58,7 +69,7 @@ var server = http.createServer(function (request, response) {
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
-  } else if (path === '/sign_up' && method === 'POST') {
+  } else if (path === '/sign_up' && method === 'POST') { //注册
     let body = []; //请求体
     request.on('data', (chunk) => {
       body.push(chunk)
@@ -123,7 +134,7 @@ var server = http.createServer(function (request, response) {
     response.write(string)
     response.end()
   } 
-  else if (path === '/sign_in' && method === 'POST') {
+  else if (path === '/sign_in' && method === 'POST') {  //登录
     let body = []; //请求体
     request.on('data', (chunk) => {
       body.push(chunk)
@@ -154,7 +165,9 @@ var server = http.createServer(function (request, response) {
         } 
       }
       if (found) {
-        response.setHeader('Set-Cookie',`sign_in_email=${email}`)
+        let sessionId = Math.random() * 100000
+        sessions[sessionId] = {sign_in_email:email}
+        response.setHeader('Set-Cookie',`sessionId=${sessionId}`)
         response.statusCode = 200
       } else {
         response.statusCode = 401
